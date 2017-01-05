58380fb8f3828ca8da5d6b4dcbaf2997172ef98f
Jack Zheng <jzheng7@stuy.edu>
2017-01-05 14:38:00 -0500

    Add scanner to read from file

--- Implementation.java
+++ Implementation.java
@@ -1,5 +1,23 @@
 public class Implementation{
 
+    private String originalText, encryptedText, decryptedText, fileName;
+    
+    public Implementation(){
+        encryptedText = "";
+	decryptedText = "";
+    }
 
+    public static void getText(){
+	try{
+	    Scanner in = new Scanner(new File(file));
+	    for(int p = 0; in.hasNext(); p++){
+		String word = in.next();
+		originalText += word + " ";
+	    }
+	}catch(FileNotFoundException e){
+	    System.out.println("Invalid filename or path");
+	    System.exit(1);
+	}
+     }
 
 }
