8f9db527e09527f851bac709e43769191c92c952
JonahSW <jsachsw@gmail.com>
2017-01-10 00:41:53 -0500

    EnigmaLitebin folder disappeared

--- Asymmetric.java
+++ Asymmetric.java
@@ -4,5 +4,8 @@ import java.io.*;
 //Asymmetric, uses a public key to encrypt and a private key to decrypt
 public class Asymmetric{
 
+    private static int publicKey;
+
+    private static int privateKey;
 }
     
