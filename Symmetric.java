9f122c09206e7e9861ed5f3164492391310bcbf8
Jack Zheng <jzheng7@stuy.edu>
2017-01-10 01:31:34 -0500

    Testing

--- Symmetric.java
+++ Symmetric.java
@@ -7,9 +7,10 @@ import javax.crypto.spec.*;
 
 public class Symmetric{
 
-    private static String algoType, originalText, modifiedText;
+    /*private static String algoType, originalText, modifiedText, exampleText;
     private static SecretKey key;
     private static byte[] unModKey;
+    private static Cipher desCipher;
 
     
     public Symmetric(String originalText, String modifiedText, String algoType){
@@ -18,26 +19,19 @@ public class Symmetric{
 	this.algoType = algoType;
     }
 
+    
     //Convert a String to SecretKey
     public static void convertToKey(String key){
 	byte[] byteKey = Base64.getDecoder().decode(key);
 	unModKey = byteKey;
     }
     
+    
     public static void encrypt(){
 	try{
-	    KeyGenerator keygenerator = KeyGenerator.getInstance(algoType);
-	    SecretKey myDesKey = keygenerator.generateKey();
-	    key = myDesKey;
-	    
-	    System.out.println(myDesKey);
-	    
-	    Cipher desCipher;
-	    desCipher = Cipher.getInstance(algoType);
+	    byte[] text = originalText.getBytes("UTF-8");
 
-	    byte[] text = originalText.getBytes("UTF8");
-
-	    desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+	    desCipher.init(Cipher.ENCRYPT_MODE, key);
 	    byte[] textEncrypted = desCipher.doFinal(text);
 
 	    String output = new String(textEncrypted);
@@ -47,18 +41,18 @@ public class Symmetric{
 	    System.out.println("You failed");
 	}
     }
+    
 
     public static void decrypt(){
 	try{
-	    Cipher desCipher;
-	    desCipher = Cipher.getInstance(algoType);
-
+	    byte[] text = modifiedText.getBytes("UTF-8");
+	    
 	    desCipher.init(Cipher.DECRYPT_MODE, key);
-	    byte[] textDecrypted = desCipher.doFinal(originalText.getBytes("UTF8"));
+	    byte[] textDecrypted = desCipher.doFinal(text);
 
 	    String output = new String(textDecrypted);
 
-	    modifiedText = output;
+	    exampleText = output;
 	}catch(Exception e){
 	    System.out.println(e);
 	}
@@ -66,15 +60,27 @@ public class Symmetric{
 	    
     public static void main(String[] args) {
 
-	Symmetric s = new Symmetric("Hello World", "", "DES");
+	Symmetric s = new Symmetric("No body can see me.", "", "AES/CBC/PKCS5Padding");
 
-	/*
+	try{
+	    KeyGenerator keygenerator = KeyGenerator.getInstance(algoType);
+	    SecretKey myDesKey = keygenerator.generateKey();
+	    key = myDesKey;
+	    desCipher = Cipher.getInstance(algoType);
+	}catch(Exception e){
+	    System.out.println("you suck");
+	}
+
+	
+	
+	
 	// create new key
 	SecretKey secretKey = KeyGenerator.getInstance(algoType).generateKey();
 	// get base64 encoded version of the key
 	String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
-	*/
 	
+
+	System.out.println(key);
 	System.out.println(originalText);
 	System.out.println("------------------------------------");
 	
@@ -87,38 +93,43 @@ public class Symmetric{
 	System.out.println("------------------------------------");
 
 	s.decrypt();
-	System.out.println(modifiedText);
-	
-	/* try{
-            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
-            SecretKey myDesKey = keygenerator.generateKey();
+	System.out.println(exampleText);*/
 
-	    System.out.println(myDesKey);
+
+	public static void main(String[] args){
 	    
-            Cipher desCipher;
-            desCipher = Cipher.getInstance("DES");
+	    try{
+		KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+		SecretKey myDesKey = keygenerator.generateKey();
+
+		System.out.println("---------------------------------");
+		System.out.println(myDesKey);
+	    
+		Cipher desCipher;
+		desCipher = Cipher.getInstance("DES");
+
+		
+		byte[] text = "No body can see me.".getBytes("UTF8");
 
 
-            byte[] text = "No body can see me.".getBytes("UTF8");
+		desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+		byte[] textEncrypted = desCipher.doFinal(text);
 
+		System.out.println("---------------------------------");
+		String s = new String(textEncrypted);
+		System.out.println(s);
 
-            desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
-            byte[] textEncrypted = desCipher.doFinal(text);
+		System.out.println("---------------------------------");
+		desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
+		byte[] textDecrypted = desCipher.doFinal(textEncrypted);
 
-            String s = new String(textEncrypted);
-            System.out.println(s);
-
-            desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
-            byte[] textDecrypted = desCipher.doFinal(textEncrypted);
-
-            s = new String(textDecrypted);
-            System.out.println(s);
-        }catch(Exception e)
-        {
-            System.out.println("Exception");
-        }
-	*/
-
-	
-    }
+		s = new String(textDecrypted);
+		System.out.println(s);
+	    }catch(Exception e)
+		{
+		    System.out.println("Exception");
+		}
+	    
+	}
 }
+
