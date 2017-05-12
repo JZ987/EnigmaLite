47e6fd7863011f1be361640f60bd6cff352f509b
Jack Zheng <jzheng7@stuy.edu>
2017-05-11 21:18:56 -0400

    Place Hash.java and Asymmetric.java inside for later

---
+++ Unfinished/Asymmetric.java
@@ -0,0 +1,38 @@
+import java.util. *;
+import java.io.*;
+
+//Asymmetric, uses a public key to encrypt and a private key to decrypt
+public class Asymmetric{
+
+    //fed from interface, else generated and printed
+    private static int publicKey;
+
+    //fed from interface, else generated and printed
+    private static int privateKey;
+
+    //fed from interface
+    private static String originalText;
+
+    //fed from interface
+    private static String modifiedText;
+
+    //Constructor
+    public Asymmetric(String originalText, String modifiedText, int publicKey, int privateKey){
+	this.originalText = originalText;
+	this.modifiedText = modifiedText;
+	this.publicKey = publicKey;
+	this.privateKey - privateKey;
+    }
+
+    private static void getPublicKey(){
+	if(publicKey == 0){
+
+	}
+    }
+
+    private static void getPrivateKey(){
+	if(privateKey == 0){
+
+	}
+    }
+}
